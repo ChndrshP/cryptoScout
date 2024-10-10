69a52f81b3a13ae21fb391f4f502fdca4d94f6ad
ChndrshP <chndrsh25@gmail.com>
2024-10-10 18:57:45 +0530

    added OTP functionality

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,14 +1,14 @@
 import nodemailer from 'nodemailer';
 
-async function sendEmail(to, subject, text){
+async function sendEmail(to, subject, text) {
     let transporter = nodemailer.createTransport({
         service: process.env.EMAIL_SERVICE,
-        auth:{
+        auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS, 
+            pass: process.env.EMAIL_PASS,
         },
     });
-    
+
     let mailOption = {
         from: process.env.EMAIL_USER,
         to: to,
@@ -16,12 +16,35 @@ async function sendEmail(to, subject, text){
         text: text,
     };
 
-    try{
+    try {
         await transporter.sendMail(mailOption);
         console.log("Email Send Successfully");
-    }catch(error){
+    } catch (error) {
         console.error('Error sending email:', error);
     }
 }
 
-export { sendEmail };
+async function sendOTP(email, otp) {
+    let transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    let mailOption = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Email verification OTP",
+        text: `Your OTP for email verification is ${otp}`,
+    };
+    try {
+        await transporter.sendMail(mailOption);
+        console.log("Email Send Successfully");
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+}
+
+export { sendEmail, sendOTP };
