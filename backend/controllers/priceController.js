798eb9dd47cc1892d5ffee4186f8303a5d98d76c
ChndrshP <chndrsh25@gmail.com>
2024-10-10 10:54:44 +0530

    Added getCoinPrice for fetching cryptocurrency data

--- backend/controllers/priceController.js
+++ backend/controllers/priceController.js
@@ -1,15 +1,22 @@
 import axios from "axios";
-import { response } from "express";
 
-async function getCoinPrice(coinId){
+async function getCoinPrice(coinId) {
     const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&ids=${coinId}`;
 
-    try{
+    try {
         const response = await axios.get(url);
-        return response.data[0];
-    }catch(error){
+        const coinData = response.data[0];
+
+        return {
+            id: coinData.id,
+            symbol: coinData.symbol,
+            name: coinData.name,
+            current_price: coinData.current_price,
+            image: coinData.image,  
+        };
+    } catch (error) {
         console.log('Error fetching coin data:', error);
     }
 }
 
-export default getCoinPrice;
+export { getCoinPrice };
