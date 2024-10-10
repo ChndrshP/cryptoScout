4a8e1100d7c593ead15ed90301b443f733a53eb2
ChndrshP <chndrsh25@gmail.com>
2024-10-10 10:57:06 +0530

    Implemented price alert system with email notifications

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,61 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import alertsRoutes from "./routes/alerts.js";
+import Alert from "./models/schema.js";
+import { getCoinPrice } from "./controllers/priceController.js";
+import { sendEmail } from "./utils/email.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("Connected to MongoDB")
-    .catch((error) => console.log("Mongo connection error:", error)));
+connectDB();
 
 app.use('/api/alerts', alertsRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listern(PORT, () => {
-    console.log("Server is running on port ${PORT}");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+async function checkPriceAlert() {
+    try {
+        const alerts = await Alert.find({ alertSent: false });
+
+        alerts.forEach(async (alert) => {
+            const coinData = await getCoinPrice(alert.coinId);
+
+            if (coinData) {
+                const currPrice = coinData.current_price;
+                const targetPrice = alert.targetPrice;
+                const halfwayPrice = (targetPrice + currPrice) / 2;
+
+                if (!alert.thresholdHit && currPrice >= halfwayPrice) {
+                    await sendEmail(
+                        alert.email,
+                        "Price Alert",
+                        `Your coin ${coinData.name} has reached halfway to your target price of $${targetPrice}. Current Price: $${currPrice}.`
+                    );
+                    alert.thresholdHit = true;
+                }
+
+                if (currPrice >= targetPrice) {
+                    await sendEmail(
+                        alert.email,
+                        "Price Alert",
+                        `Your coin ${coinData.name} has reached the target price of $${targetPrice}. Current Price: $${currPrice}.`
+                    );
+                    alert.alertSent = true;
+                }
+
+                await alert.save();
+            }
+        });
+    } catch (error) {
+        console.error("Error checking price alerts:", error);
+    }
+}
+
+setInterval(checkPriceAlert, 100000);
