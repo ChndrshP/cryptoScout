8ff66fcb7f3646367958a9098cb0b1c54f690d02
ChndrshP <chndrsh25@gmail.com>
2024-10-12 11:05:33 +0530

    added search route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import Alertrouter from "./routes/alertRouter.js";
 import userRouter from "./routes/authRouter.js";
+import searchRouter from "./routes/searchRouter.js";
 import {startPriceCheckInterval} from "./controllers/priceAlerts.js";
 
 
@@ -15,6 +16,7 @@ connectDB();
 
 app.use('/api/alerts', Alertrouter);
 app.use('/api/auth', userRouter);
+app.use('/api', searchRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
